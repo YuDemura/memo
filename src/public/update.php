a3315ca45db650854807ee8a0e5015e26b2e81d0
YuDemura <Yu.demura1214@gmail.com>
2022-06-02 17:32:05 +0900

    メモ編集関数化

--- src/public/update.php
+++ src/public/update.php
@@ -1,23 +1,12 @@
 <?php
-$dbUserName = 'root';
-$dbPassword = 'password';
-$pdo = new PDO(
-    'mysql:host=mysql; dbname=memo; charset=utf8',
-    $dbUserName,
-    $dbPassword
-);
+require_once(__DIR__ . '/../app/Lib/updateMemo.php');
+require_once(__DIR__ . '/../app/Lib/redirect.php');
 
 $id = filter_input(INPUT_POST, 'id');
 $title = filter_input(INPUT_POST, 'title');
 $content = filter_input(INPUT_POST, 'content');
 
-$sql = 'UPDATE pages SET title=:title, content=:content WHERE id = :id';
-$statement = $pdo->prepare($sql);
-$statement->bindValue(':id', $id, PDO::PARAM_INT);
-$statement->bindValue(':title', $title, PDO::PARAM_STR);
-$statement->bindValue(':content', $content, PDO::PARAM_STR);
-$statement->execute();
+updateMemo($title, $content, $id);
 
-header('Location: ./index.php');
-exit();
+redirect('./index.php');
 ?>
