4725a5be63a80fe9f932baf49cc19e03f7755709
YuDemura <Yu.demura1214@gmail.com>
2022-06-27 21:03:21 +0900

    Daoファイル作成に伴い削除

--- src/app/Lib/pdoInit.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function pdoInit(): PDO
-{
-    $dbUserName = 'root';
-    $dbPassword = 'password';
-    $pdo = new PDO("mysql:host=mysql; dbname=memo; charset=utf8", $dbUserName, $dbPassword);
-	return $pdo;
-}
